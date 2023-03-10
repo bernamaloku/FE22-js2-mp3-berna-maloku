dc306953c3913709752d94521856695bfc99943c
Berna <106542048+bernamaloku@users.noreply.github.com>
2023-03-10 15:53:43 +0100

    Update kundvagn.js

--- js/kundvagn.js
+++ js/kundvagn.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-//Detta är JavaScript-kod som hämtar data från en Firebase Realtime Database och använder den för att skapa en lista med produkter.
+//detta hämtar data från en firebase realtime database och använder den för att skapa en lista med produkter.
 const apiUrl =
   "https://miniprojekt3-177bf-default-rtdb.europe-west1.firebasedatabase.app/cart.json";
 let products = [];
